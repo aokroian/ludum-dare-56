48550ea9d65cf5b199d9e10fcb2d05c7626bcef5
aokroian <49920714+aokroian@users.noreply.github.com>
2024-10-06 17:24:45 +0300

    props WIP

--- Assets/_Game/Scripts/Level/Prop.cs
+++ Assets/_Game/Scripts/Level/Prop.cs
@@ -1,9 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Level
 {
     public class Prop : MonoBehaviour
     {
-        [field: SerializeField] public int Size { get; private set; }
+        [field: SerializeField] public SphereCollider Bounds { get; private set; }
+        [SerializeField] private List<GameObject> allVariations;
+
+        private GameObject _currentVariation;
+
+        private static bool IsDebug => true;
+
+        private void Awake()
+        {
+            if (!_currentVariation)
+                SelectRandomVariation();
+        }
+
+        public void SelectRandomVariation()
+        {
+            var exceptCurrent = _currentVariation
+                ? allVariations.Where(v => v != _currentVariation).ToList()
+                : allVariations;
+            var randomIndex = Random.Range(0, exceptCurrent.Count);
+            _currentVariation = exceptCurrent[randomIndex];
+
+            foreach (var variation in allVariations)
+                variation.SetActive(variation == _currentVariation);
+        }
+
+#if UNITY_EDITOR
+        [Button]
+        public void FillVariations()
+        {
+            allVariations.Clear();
+            var variationsParent = transform.Find("Variations");
+            foreach (Transform variation in variationsParent)
+                allVariations.Add(variation.gameObject);
+        }
+
+        private void OnDrawGizmos()
+        {
+            // draw socket size using bounds
+            if (!IsDebug || !Bounds)
+                return;
+            Gizmos.color = Color.green;
+            var worldSpaceBoundsCenter = transform.TransformPoint(Bounds.center);
+            Gizmos.DrawWireSphere(worldSpaceBoundsCenter, Bounds.radius);
+        }
+#endif
     }
 }
