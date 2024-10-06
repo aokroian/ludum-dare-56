e30f1e24d3772cfb5dffed5d9273f3c5b2eb2590
aokroian <49920714+aokroian@users.noreply.github.com>
2024-10-06 20:14:38 +0300

    some progress on main menu

--- Assets/_Game/Scripts/Menu/MainMenuController.cs
+++ Assets/_Game/Scripts/Menu/MainMenuController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using _GameTemplate.Scripts.SceneManagement;
+﻿using _GameTemplate.Scripts.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,24 +8,57 @@ namespace Menu
     {
         [SerializeField] private Button startGameBtn;
         [SerializeField] private Button settingsBtn;
+        [SerializeField] private Button backFromSettingsBtn;
         [SerializeField] private Button quitBtn;
-        
+        [SerializeField] private Button creditsBtn;
+        [SerializeField] private Button backFromCreditsBtn;
+
         [SerializeField] private RectTransform settingsPanel;
+        [SerializeField] private RectTransform creditsPanel;
+
 
         private void Start()
         {
             startGameBtn.onClick.AddListener(StartGame);
             quitBtn.onClick.AddListener(QuitGame);
+            settingsBtn.onClick.AddListener(OpenSettings);
+            backFromSettingsBtn.onClick.AddListener(CloseSettings);
+            creditsBtn.onClick.AddListener(OpenCredits);
+            backFromCreditsBtn.onClick.AddListener(CloseCredits);
+
+#if UNITY_WEBGl
+            quitBtn.gameObject.SetActive(false);
+#endif
         }
-        
+
         private void StartGame()
         {
             CustomSceneManager.LoadScene("Game");
         }
-        
+
         private void QuitGame()
         {
             Application.Quit();
+        }
+
+        private void OpenSettings()
+        {
+            settingsPanel.gameObject.SetActive(true);
+        }
+
+        private void CloseSettings()
+        {
+            settingsPanel.gameObject.SetActive(false);
+        }
+
+        private void OpenCredits()
+        {
+            creditsPanel.gameObject.SetActive(true);
+        }
+
+        private void CloseCredits()
+        {
+            creditsPanel.gameObject.SetActive(false);
         }
     }
 }
