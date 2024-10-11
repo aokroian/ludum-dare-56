a944f5f8a69a445e56a8f55f93939bd1ff736932
GastarTestProjects <xe7272@gmail.com>
2024-10-11 14:45:52 +0400

    Add menu item to clear playerPrefs

---
+++ Assets/_Game/Scripts/EditorScripts/EditorUtils.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EditorScripts
+{
+    public class EditorUtils
+    {
+        [MenuItem("Custom/Clear All PlayerPrefs")]
+        public static void ClearAllPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+            Debug.Log("All PlayerPrefs have been cleared.");
+        }
+    }
+}
